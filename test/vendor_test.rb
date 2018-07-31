93ad3e2869cc154a1e68ab8cb5b3f3aa705da1b5
Jesse Ling <jesse.ryan.ling@gmail.com>
2018-07-31 09:16:47 -0600

    Initial commit

---
+++ test/vendor_test.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/vendor'
+
+class VendorTest < Minitest::Test
+
+  def setup
+    @vendor = Vendor.new("Rocky Mountain Fresh")
+  end
+
+  def test_it_exists
+    assert_instance_of Vendor, @vendor
+  end
+
+  def test_it_has_a_name
+    assert_equal "Rocky Mountain Fresh", @vendor.name
+  end
+
+  def test_inventory_starts_as_empty_hash
+    assert_equal ({}), @vendor.inventory
+  end
+
+  def test_check_stock_returns_a_default_value_if_not_found
+    assert_equal 0, @vendor.check_stock("Peaches")
+  end
+
+  def test_it_can_stock_items
+    assert_equal 0, @vendor.check_stock("Peaches")
+    @vendor.stock("Peaches", 30)
+    assert_equal 30, @vendor.check_stock("Peaches")
+    @vendor.stock("Peaches", 25)
+    assert_equal 55, @vendor.check_stock("Peaches")
+  end
+
+end
